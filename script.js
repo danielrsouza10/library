167ab4afbbd0bc7bed601cfcbfe458c8b81a9e91
Daniel Souza <danielrsouza10@gmail.com>
2023-05-14 15:04:04 -0300

    Create a function to push the new books to the array library

--- script.js
+++ script.js
@@ -7,22 +7,20 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
-function addToLibrary() {
+function addToLibrary(title, author, pages, read) {
   //a function that do library.push
+  let book = new Book(title, author, pages, read);
+  library.push(book);
 }
 
-const book1 = new Book("The lord of the rings", "Tolkien", 600, "yes");
-const book2 = new Book("O mundo de sofia", "Jostein Gaarden", 500, "yes");
-console.log(book1.title);
-console.log(book2.author);
+addToLibrary("The lord of the rings", "Tolkien", 600, "yes");
+addToLibrary("O mundo de sofia", "Jostein Gaarden", 500, "yes");
 
-library.push(book1);
-library.push(book2);
 console.log(library);
 
-const cardContainer = document.querySelector("#card-container");
-const cards = document.createElement("div");
-cards.className = "cards";
-cards.innerHTML = "Title: " + book1.title;
+// const cardContainer = document.querySelector("#card-container");
+// const cards = document.createElement("div");
+// cards.className = "cards";
+// cards.innerHTML = "Title: " + book1.title;
 
-cardContainer.appendChild(cards);
+// cardContainer.appendChild(cards);
