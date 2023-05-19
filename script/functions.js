e84e3067f9ecf712937548905f926f8e39004876
Daniel Souza <danielrsouza10@gmail.com>
2023-05-19 11:54:26 -0300

    page is working properly. Time to do some style!

--- script/functions.js
+++ script/functions.js
@@ -1,40 +1,77 @@
 let library = [];
+let queue = [];
 
-function Book(title, author, pages, read) {
+//constructor
+function Book(title, author, pages) {
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.read = read;
+  // this.read = read;
 }
 
-function addToLibrary(title, author, pages, read) {
+function addToLibrary(title, author, pages) {
   //a function that do library.push
-  let book = new Book(title, author, pages, read);
+  let book = new Book(title, author, pages);
   library.push(book);
+  queue = book;
 }
 
-addToLibrary("O Senhor dos Anéis", "Tolkien", 600, "lido");
-addToLibrary("O Hobbit", "Tolkien", 350, "lido");
-addToLibrary("Contos Inacabados", "Tolkien", 500, "Não lido");
-
-library.forEach((element) => {
-  for (let i = 0; i <= library.length; i++) {
+//a function to show all the books insert by default in the script
+function showLibrary() {
+  library.forEach((element) => {
     const list = document.querySelector("#card-container");
     const book = document.createElement("ul");
     const titleBook = document.createElement("li");
     const authorBook = document.createElement("li");
     const pagesBook = document.createElement("li");
-    const readBook = document.createElement("li");
+    // const readBook = document.createElement("li");
 
-    titleBook.innerText = "Title: " + library[i].title;
-    authorBook.innerText = "Author: " + library[i].author;
-    pagesBook.innerText = "Number of pages: " + library[i].pages;
-    readBook.innerText = "Read: " + library[i].read;
+    titleBook.innerText = "Title: " + element.title;
+    authorBook.innerText = "Author: " + element.author;
+    pagesBook.innerText = "Number of pages: " + element.pages;
+    // readBook.innerText = "Read: " + element.read;
 
     list.appendChild(book);
     book.appendChild(titleBook);
     book.appendChild(authorBook);
     book.appendChild(pagesBook);
-    book.appendChild(readBook);
-  }
+    // book.appendChild(readBook);
+  });
+}
+
+function showLastBook() {
+  const list = document.querySelector("#card-container");
+  const book = document.createElement("ul");
+  const titleBook = document.createElement("li");
+  const authorBook = document.createElement("li");
+  const pagesBook = document.createElement("li");
+  // const readBook = document.createElement("li");
+
+  titleBook.innerText = "Title: " + queue.title;
+  authorBook.innerText = "Author: " + queue.author;
+  pagesBook.innerText = "Number of pages: " + queue.pages;
+  // readBook.innerText = "Read: " + element.read;
+
+  // book.appendChild(readBook);
+  list.appendChild(book);
+  book.appendChild(titleBook);
+  book.appendChild(authorBook);
+  book.appendChild(pagesBook);
+}
+
+//some books just to test
+addToLibrary("O Senhor dos Anéis", "Tolkien", 600, "lido");
+addToLibrary("O Hobbit", "Tolkien", 350, "lido");
+addToLibrary("Contos Inacabados", "Tolkien", 500, "Não lido");
+
+showLibrary();
+
+const button = document.querySelector("#add-to-library");
+button.addEventListener("click", () => {
+  let bookTitle = document.querySelector("#bookTitle").value;
+  let bookAuthor = document.querySelector("#bookAuthor").value;
+  let bookPages = document.querySelector("#bookPages").value;
+  addToLibrary(bookTitle, bookAuthor, bookPages);
+
+  showLastBook();
 });
